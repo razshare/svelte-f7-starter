3c169cb71cea10961f7d1dbb1162532e78f7c2b3
Razvan <tangent.jotey@gmail.com>
2021-07-10 17:48:16 +0200

    Update app.ts
    fixed an issue where the client  would try to fetch main.version when offline and break the application.

--- src/lib/scripts/app/app.ts
+++ src/lib/scripts/app/app.ts
@@ -1,17 +1,18 @@
 import { findServiceWorker } from "./findServiceWorker"
 import localVersion from '../../stores/localVersion'
 import message from "../../stores/message"
-import remoteVersionFilename from '../../../main.version?url'
+
 
 async function initWebApp():Promise<void>{
     let $localVersion:string
     localVersion.subscribe($=>$localVersion=$)()
-    let remoteVersion:any
-
+    
     try{
+        let remoteVersionFilename = (await import('../../../main.version?url')).default
         console.log({remoteVersionFilename})
-        remoteVersion = await fetch(remoteVersionFilename)
-        remoteVersion = await remoteVersion.text()
+        let response = await fetch(remoteVersionFilename)
+        let remoteVersion = await response.text()
+        setup($localVersion,remoteVersion);
     }catch(e){
         /**
          * This is very important.
@@ -26,53 +27,59 @@ async function initWebApp():Promise<void>{
          * remote version should be set the same as the local version.
          */
         
-        remoteVersion = $localVersion
+        setup($localVersion,$localVersion);
     }
 
+}
 
+function sync($localVersion:string, remoteVersion:string):void{
+    if($localVersion !== remoteVersion){
+        //location.reload(true);
+        message.set({
+            text: `New version available ${remoteVersion}`,
+            timeout: 1000*60*30, //30 minutes
+            buttons:[
+                {
+                    text:'Update',
+                    action:()=>{
+                        message.set(null)   //this will close the snackbar menu
+                        localStorage.clear()
+                        console.log(`Updating from version ${$localVersion} to version ${$remoteVersion}`)
+                        if(window.caches)
+                            caches.keys().then((keyList) => {
+                                console.log({keyList});
+                                
+                                Promise.all(keyList.map((key) => caches.delete(key)))
+                            });
+                        if(window.navigator && window.navigator.serviceWorker)
+                            window.navigator.serviceWorker.getRegistrations().then(function(registrations) {
+                                for(let registration of registrations) {
+                                    registration.unregister()
+                                }
+                            })
+                        localVersion.set(remoteVersion)
+        
+                        location.reload(true)
+                    }
+                },
+                {
+                    text:'Close',
+                    action:()=>message.set(null),
+                }
+            ]
+        });
+        
+    }
+}
+
+function setup($localVersion:string,remoteVersion:string):void{
     try{
         console.log("versions:",{
             "local":$localVersion,
             "remote":remoteVersion
         })
         
-        if($localVersion !== remoteVersion){
-            //location.reload(true);
-            message.set({
-                text: `New version available ${remoteVersion}`,
-                timeout: 1000*60*30, //30 minutes
-                buttons:[
-                    {
-                        text:'Update',
-                        action:()=>{
-                            message.set(null)   //this will close the snackbar
-                            localStorage.clear()
-                            console.log(`Updating from version ${$localVersion} to version ${remoteVersion}`)
-                            if(window.caches)
-                                caches.keys().then((keyList) => {
-                                    console.log({keyList});
-                                    
-                                    Promise.all(keyList.map((key) => caches.delete(key)))
-                                });
-                            if(window.navigator && window.navigator.serviceWorker)
-                                window.navigator.serviceWorker.getRegistrations().then(function(registrations) {
-                                    for(let registration of registrations) {
-                                        registration.unregister()
-                                    }
-                                })
-                            localVersion.set(remoteVersion)
-            
-                            location.reload(true)
-                        }
-                    },
-                    {
-                        text:'Close',
-                        action:()=>message.set(null),
-                    }
-                ]
-            });
-            
-        }
+        sync($localVersion,remoteVersion);
         
         if(window.navigator && window.navigator.serviceWorker) 
             navigator.serviceWorker.getRegistrations().then(findServiceWorker);
